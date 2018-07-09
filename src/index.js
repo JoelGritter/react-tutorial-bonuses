de0290f712c065a074d57996b86aed2fdf259f41
JoelGritter <joel@gritter.ca>
2018-07-09 10:13:24 -0400

    Add italics to winning squares

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
+  if(props.modifier){
+    return <button className="square" onClick={props.onClick}><i>{props.value}</i></button>
+  } else {
+    return <button className="square" onClick={props.onClick}>{props.value}</button>
+  }
 }
 
 class Board extends React.Component {
@@ -16,6 +16,7 @@ class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        modifier={(this.props.modifier !== null && this.props.modifier.indexOf(i) !== -1)? true : false}
       />
     );
   }
@@ -97,7 +98,7 @@ class Game extends React.Component {
 
     let status;
     if(winner){
-      status = 'Winner : ' + winner;
+      status = 'Winner : ' + winner[0];
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -108,6 +109,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            modifier={(winner)? winner[1] : null}
           />
         </div>
         <div className="game-info">
@@ -140,7 +142,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], lines[i]];
     }
   }
   return null;
